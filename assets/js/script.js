fa3d3842014cc85a76e3505aa6799b941a3b498f
Todd Croak-Falen <toddcf@gmail.com>
2017-03-21 19:47:02 -0700

    Created nearly identical functions in ES5 and ES6.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@
 // ES5:
 
 // Variables declared in ES5 are "function scoped," which means they can be changed.
-
+/*
 var name5 = "Jane Smith";
 var age5 = 23;
 name5 = "Jane Miller";
@@ -28,3 +28,31 @@ let age6 = 23;
 // This won't work:
 name6 = "Jane Miller";
 console.log(name6);
+*/
+
+
+// ES5:
+
+function driversLicense5( passedTest ) {
+	if ( passedTest ) {
+		var firstName = "John";
+		var yearOfBirth = 1990;
+
+		console.log( firstName + ", born in " + yearOfBirth + ", is now officially allowed to drive a car." );
+	}
+};
+
+driversLicense5( true );
+
+// ES6:
+
+function driversLicense6( passedTest ) {
+	if ( passedTest ) {
+		let firstName = "John";
+		const yearOfBirth = 1990;
+
+		console.log( firstName + ", born in " + yearOfBirth + ", is now officially allowed to drive a car." );
+	}
+};
+
+driversLicense6( true );
