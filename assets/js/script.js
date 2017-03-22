d1e0e8446d6d6520435ef0df507c26bb88e595f2
Todd Croak-Falen <toddcf@gmail.com>
2017-03-21 19:09:40 -0700

    Added basic notes on ES6.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-alert("script.js file is linked!");
+// ES2015 (short for ECMAScript 2015) is the official name for the new version of JavaScript.
+// However, everyone just calls it ES6, because it came after ES5.
+// ES5 is currently supported in all modern browsers.
+// ES6 has partial support in modern browsers, and no support in older browsers.
+// ES2016 is already out, but has almost no browser support at this time.
+// Use a transpiler that converts ES6 code back to ES5 to ensure browser support.
