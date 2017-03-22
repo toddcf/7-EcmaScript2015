f3e67dfecdafeeadb3192092e565ed0470bff3ea
Todd Croak-Falen <toddcf@gmail.com>
2017-03-21 19:38:51 -0700

    Tested the difference between var, let, and const.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,27 @@
 // ES6 has partial support in modern browsers, and no support in older browsers.
 // ES2016 is already out, but has almost no browser support at this time.
 // Use a transpiler that converts ES6 code back to ES5 to ensure browser support.
+
+
+// Variable Declaractions: LET and CONST
+
+// ES5:
+
+// Variables declared in ES5 are "function scoped," which means they can be changed.
+
+var name5 = "Jane Smith";
+var age5 = 23;
+name5 = "Jane Miller";
+console.log(name5);
+
+// ES6:
+
+// Variables declared in ES6 are "block scoped," and use either LET or CONST.
+// let is like var, and can be changed later.
+// const is for values we don't want to change.
+
+const name6 = "Jane Smith";
+let age6 = 23;
+// This won't work:
+name6 = "Jane Miller";
+console.log(name6);
