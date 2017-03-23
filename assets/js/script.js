274dcaf5ba65694efca23420678637e601ff5c85
Todd Croak-Falen <toddcf@gmail.com>
2017-03-23 14:58:42 -0700

    Changed 'this' to 'self' in the function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -197,7 +197,7 @@ var box5 = {
 	clickMe: function() {
 		var self = this;
 		document.querySelector( ".green" ).addEventListener( "click", function() {
-			var str = "This is box number " + this.position + ", and it is " + this.color + ".";
+			var str = "This is box number " + self.position + ", and it is " + self.color + ".";
 			alert( str );
 		});
 	}
