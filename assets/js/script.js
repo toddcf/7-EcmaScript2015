3a3e1d8d13f6c3a1aca55e193d572e74e9981c0f
Todd Croak-Falen <toddcf@gmail.com>
2017-03-23 14:57:51 -0700

    Added workaround (var self = this) inside the clickMe function, since the this keyword was pointing to the window object.

--- assets/js/script.js
+++ assets/js/script.js
@@ -195,6 +195,7 @@ var box5 = {
 	color: "green",
 	position: 1,
 	clickMe: function() {
+		var self = this;
 		document.querySelector( ".green" ).addEventListener( "click", function() {
 			var str = "This is box number " + this.position + ", and it is " + this.color + ".";
 			alert( str );
