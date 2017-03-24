db90a25348b34d33240ccfc3688fe00a3e523d37
Todd Croak-Falen <toddcf@gmail.com>
2017-03-24 16:26:01 -0700

    Added spread operator.

--- assets/js/script.js
+++ assets/js/script.js
@@ -408,6 +408,49 @@ function addFourAges( a, b, c, d ) {
 var sum1 = addFourAges( 18, 30, 12, 21 );
 console.log( sum1 );
 
+// What if the ages were in an array? Use the "apply" method.
+
+// ES5:
+var ages = [ 18, 30, 12, 21 ];
+// In using the apply method, we must specify "this". But in this case we don't need it, so we can just say it's null.
+var sum2 = addFourAges.apply( null, ages );
+console.log( sum2 );
+
+// ES6:
+
+// The spread operator is the ellipses:
+const sum3 = addFourAges( ... ages );
+// It will expand the ages array into its components.
+console.log( sum3 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
