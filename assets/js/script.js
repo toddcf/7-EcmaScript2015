cff30372cbba8fbefa28309de76b604cc68fdfa9
Todd Croak-Falen <toddcf@gmail.com>
2017-03-23 17:35:42 -0700

    ES6 version of the same thing.

--- assets/js/script.js
+++ assets/js/script.js
@@ -247,7 +247,7 @@ function Person( name ) {
 };
 
 // ES5:
-
+/*
 Person.prototype.myFriends5 = function( friends ) {
 	var arr = friends.map( function( el ) {
 		return this.name + " is friends with " + el
@@ -258,8 +258,19 @@ Person.prototype.myFriends5 = function( friends ) {
 
 var friends = ["Bob", "Jane", "Mark"];
 new Person( "John" ).myFriends5( friends );
+*/
+// ES6:
 
+Person.prototype.myFriends6 = function( friends ) {
+	var arr = friends.map( ( el ) => {
+		return this.name + " is friends with " + el
+	});
 
+	console.log( arr );
+}
+
+var friends = ["Bob", "Jane", "Mark"];
+new Person( "John" ).myFriends6( friends );
 
 
 
