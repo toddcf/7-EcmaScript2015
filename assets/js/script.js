522ef4fd0e00abc7c55aee87eb2ef6093be7dd34
Todd Croak-Falen <toddcf@gmail.com>
2017-03-24 18:05:12 -0700

    ES5 function to convert arguments to an array, loop through it, and console log whether or not each person is over the age of 18.

--- assets/js/script.js
+++ assets/js/script.js
@@ -400,7 +400,7 @@ console.log( ages[ full.indexOf( true ) ] );
 // Spread Operator
 
 // A convenient way to expand elements of an array, like arguments and function calls.
-
+/*
 function addFourAges( a, b, c, d ) {
 	return a + b + c+ d;
 };
@@ -446,8 +446,37 @@ const all = [ h, ... boxes ];
 
 // Convert nodelist to array:
 Array.from( all ).forEach( cur => cur.style.color = "purple" );
+*/
+
+// Rest Parameters
+
+// These allow us to pass an arbitrary number of arguments into a function.
+// These look like spread operators but are the exact opposite.
+// Whereas the spread operator expands an array into its components, rest parameters receive a couple single values and transforms them into an array when we call a function with multiple parameters.
 
 
+// Example:
+// We have a function that receives an arbitrary number of years, and then prints to the console whether each person is over 18 years of age.
+// Note that "arguments" is like the "this" keyword.
+// Although "arguments" looks a lot like an array, it is actually an object.
+// Arrays have the array function constructor as their prototype, but "arguments" has the object prototype.
+// So "arguments" is an array-like structure, but is not actually an array.
+// If you want to use it as an array (to loop through it, for example), you must first convert it into an array.
+
+// ES5:
+function isFullAge5() {
+	// console.log( arguments );
+	// Convert to an array and store in new variable:
+	var argsArray = Array.prototype.slice.call( arguments );
+	// Now that it's been converted into an array, you can loop through it:
+	argsArray.forEach( function( cur ) {
+		// cur comes from the current array, which comes from the arguments, which are all the numbers that are passed into the function when it's called:
+		console.log( ( 2017 - cur) >= 18 );
+	})
+};
+
+// Pass some years into the function:
+isFullAge5( 1990, 2001, 1965 );
 
 
 
